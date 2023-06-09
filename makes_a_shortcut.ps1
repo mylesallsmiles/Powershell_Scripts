24e778a9c3769b7996e8837d5a518acfccbb7762
Myles Thompson <mylest@CCOI.local>
2023-06-09 10:36:48 -0500

    makes_a_shortcut.ps1

---
+++ makes_a_shortcut.ps1
@@ -0,0 +1,7 @@
+﻿$wshShell = New-Object -ComObject "WScript.Shell"
+$urlShortcut = $wshShell.CreateShortcut(
+  (Join-Path $wshShell.SpecialFolders.Item("AllUsersDesktop") "HelpDesk.url")
+)
+$urlShortcut.TargetPath = "#Link"
+$urlShortcut.Save()
+
